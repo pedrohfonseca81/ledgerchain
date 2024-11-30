126c2c0fd4cf919fe11ec7436119bfb3d7de73ae
tyde81 <pedrohfonseca.1981@gmail.com>
2024-11-30 00:00:02 -0300

    refactor: add types and config structure

--- app/Blockchain/ProofOfWork.php
+++ app/Blockchain/ProofOfWork.php
@@ -2,6 +2,8 @@
 
 namespace Ledgerchain\Blockchain;
 
+use DateTime;
+
 class ProofOfWork
 {
     public function hashMatchesDifficulty($hash, $difficulty): bool
@@ -17,40 +19,37 @@ class ProofOfWork
     {
         $binary = '';
         for ($i = 0; $i < strlen($hash); $i++) {
-            $binary .= str_pad(base_convert(ord($hash[$i]), 10, 16, 2), 4, '0', STR_PAD_LEFT);
+            $binary .= str_pad(base_convert(ord($hash[$i]), 10, 16), 4, '0', STR_PAD_LEFT);
         }
         return $binary;
     }
 
 
-    public function findBlock($index, $previousHash, $timestamp, $data, $difficulty): Block
+    public function findBlock(int $index, string $previousHash, DateTime $timestamp, string $data, int $difficulty): Block
     {
         $nonce = 0;
 
-        while (true){
+        while (true) {
             $hash = $this->calculateHash($index, $previousHash, $timestamp, $data, $difficulty, $nonce);
-            if ($this->hashMatchesDifficulty($hash, $difficulty)){
-                return new Block($index, $hash, $previousHash, $timestamp, $data, $difficulty, $nonce);
+            if ($this->hashMatchesDifficulty($hash, $difficulty)) {
+                return new Block($index, $data, $timestamp, $hash, $previousHash, $difficulty, $nonce);
             }
             $nonce++;
         }
     }
 
-    private function calculateHash($index, $previousHash, $timestamp, $data, $difficulty, int $nonce): string
+    private function calculateHash(int $index, string $previousHash, DateTime $timestamp, string $data, int $difficulty, int $nonce): string
     {
-        $input = $index . $previousHash . $timestamp . $data . $difficulty . $nonce;
+        $input = $index . $previousHash . $timestamp->getTimestamp() . $data . $difficulty . $nonce;
 
         return hash('sha256', $input);
     }
-
-    const BLOCK_GENERATION_INTERVAL = 10; // segundos
-    const DIFFICULTY_ADJUSTMENT_INTERVAL = 10; // blocos
 
     public function getDifficulty(array $aBlockchain): int
     {
         $latestBlock = $aBlockchain[count($aBlockchain) - 1];
 
-        if ($latestBlock['index'] % self::DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && $latestBlock['index'] !== 0) {
+        if ($latestBlock['index'] % Config::DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && $latestBlock['index'] !== 0) {
             return $this->getAdjustedDifficulty($latestBlock, $aBlockchain);
         } else {
             return $latestBlock['difficulty'];
@@ -59,8 +58,8 @@ class ProofOfWork
 
     public function getAdjustedDifficulty(array $latestBlock, array $aBlockchain): int
     {
-        $prevAdjustmentBlock = $aBlockchain[count($aBlockchain) - self::DIFFICULTY_ADJUSTMENT_INTERVAL];
-        $timeExpected = self::BLOCK_GENERATION_INTERVAL * self::DIFFICULTY_ADJUSTMENT_INTERVAL;
+        $prevAdjustmentBlock = $aBlockchain[count($aBlockchain) - Config::DIFFICULTY_ADJUSTMENT_INTERVAL];
+        $timeExpected = Config::BLOCK_GENERATION_INTERVAL * Config::DIFFICULTY_ADJUSTMENT_INTERVAL;
         $timeTaken = $latestBlock['timestamp'] - $prevAdjustmentBlock['timestamp'];
 
         if ($timeTaken < $timeExpected / 2) {
---
+++ app/Config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ledgerchain\Blockchain;
+
+class Config
+{
+    const BLOCK_GENERATION_INTERVAL = 10;
+    const DIFFICULTY_ADJUSTMENT_INTERVAL = 10;
+}
